33a21862442c58d1df3405d035344503ae59095d
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-02-03 21:31:31 +0000

    Confirm local dev is working

--- src/strategyTemplateSeaDogDiscountScheme.ts
+++ src/strategyTemplateSeaDogDiscountScheme.ts
@@ -1,10 +1,24 @@
-import { Client } from '@umerx/umerx-blackdog-configurator-client-typescript';
+import { Client as BlackdogConfiguratorClient } from '@umerx/umerx-blackdog-configurator-client-typescript';
 import { AlpacaClient, AlpacaStream } from '@umerx/alpaca';
 
 const alpacaClientCredentialsKey =
     process.env.ALPACA_CLIENT_CREDENTIALS_KEY ?? '';
 const alpacaClientCredentialsSecret =
     process.env.ALPACA_CLIENT_CREDENTIALS_SECRET ?? '';
+
+const blackdogBlackdogConfiguratorClientScheme =
+    process.env.BLACKDOG_CONFIGURATOR_CLIENT_SCHEME ?? '';
+const blackdogBlackdogConfiguratorClientHost =
+    process.env.BLACKDOG_CONFIGURATOR_CLIENT_HOST ?? '';
+const blackdogBlackdogConfiguratorClientPort =
+    process.env.BLACKDOG_CONFIGURATOR_CLIENT_PORT ?? '';
+const blackdogBlackdogConfiguratorClientPath =
+    process.env.BLACKDOG_CONFIGURATOR_CLIENT_PATH ?? '';
+const blackdogBlackdogConfiguratorClientBaseUrl = `${blackdogBlackdogConfiguratorClientScheme}://${blackdogBlackdogConfiguratorClientHost}${
+    undefined === blackdogBlackdogConfiguratorClientPort
+        ? ''
+        : `:${blackdogBlackdogConfiguratorClientPort}`
+}${blackdogBlackdogConfiguratorClientPath}`;
 
 const alpacaClient = new AlpacaClient({
     credentials: {
@@ -14,4 +28,13 @@ const alpacaClient = new AlpacaClient({
     rate_limit: true,
 });
 
+const blackdogBlackdogConfiguratorClient =
+    new BlackdogConfiguratorClient.ClientImpl(
+        blackdogBlackdogConfiguratorClientBaseUrl
+    );
+
 const account = await alpacaClient.getAccount();
+
+console.log(JSON.stringify(account, null, 2));
+const symbols = await blackdogBlackdogConfiguratorClient.symbol().getMany({});
+console.log(JSON.stringify(symbols, null, 2));
