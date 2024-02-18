adad5dbf88fb53c266b1c869ddc52511d3000bf2
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-02-18 22:47:02 +0000

    Change var to const

--- src/strategyTemplateSeaDogDiscountScheme.ts
+++ src/strategyTemplateSeaDogDiscountScheme.ts
@@ -301,13 +301,13 @@ async function resolveOpenPosition(
 }
 
 function bankersRounding(num: number, decimalPlaces: number = 2): number {
-    var d = decimalPlaces;
-    var m = Math.pow(10, d);
-    var n = +(d ? num * m : num).toFixed(8); // Avoid rounding errors
-    var i = Math.floor(n),
+    const d = decimalPlaces;
+    const m = Math.pow(10, d);
+    const n = +(d ? num * m : num).toFixed(8); // Avoid rounding errors
+    const i = Math.floor(n),
         f = n - i;
-    var e = 1e-8; // Allow for rounding errors in f
-    var r =
+    const e = 1e-8; // Allow for rounding errors in f
+    const r =
         f > 0.5 - e && f < 0.5 + e ? (i % 2 == 0 ? i : i + 1) : Math.round(n);
 
     return d ? r / m : r;
