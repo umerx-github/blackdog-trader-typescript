08a04583665e5347946a4b4c8a07723a661e3858
Andrew Dawes <andrewjamesdawes@gmail.com>
2024-02-25 17:12:01 +0000

    Publish

--- dev/publish.sh
+++ dev/publish.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
-
-npm run build && git add . && git commit -m"$1" && npm version patch && npm publish --access public
+# Confirm the first argument is one of major, minor, or patch
+if [[ ! $1 =~ ^(major|minor|patch)$ ]]; then
+    echo "First argument must be one of major, minor, or patch"
+    exit 1
+fi
+# Confirm the second argument is not empty
+if [[ -z $2 ]]; then
+    echo "Second argument must be a commit message"
+    exit 1
+fi
+git add . && git commit --allow-empty -m"$2"
+version=$(npm version "$1")
+echo "npm version is now $version"
+git push && git push origin "$version"
